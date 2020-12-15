3e04352db3d48a7030142eb094b496636ad4b70e
KC <krkeco@gmail.com>
2020-12-15 14:52:56 -0800

    hrefs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import { Animate, AnimateGroup } from "react-simple-animate";
+import Card from './components/Card'
+import './App.css'
+import logo from './logo.svg'
+
+const App = () => (
+  <div className='app'>
+    <div className='container'>
+      <AnimateGroup play>
+        <Animate start={{ opacity: 0 }} end={{ opacity: 1 }} sequenceIndex={0}>
+          <Card name="KC" x={'20vw'} y={'25vh'}/>
+        </Animate>
+        <Animate start={{ opacity: 0 }} end={{ opacity: 1 }} sequenceIndex={1}>
+        <Card name="Hannah" href='https://hannah.na-cole.com' x={'40vw'} y={'65vh'}/>
+        </Animate>
+        <Animate start={{ opacity: 0 }} end={{ opacity: 1 }} sequenceIndex={2}>
+        <Card name="Callings" href='http://tco.na-cole.com' x={'70vw'} y={'40vh'}/>
+        </Animate>
+      </AnimateGroup>
+      {/* <img src={logo} className='logo' alt='logo'/> */}
     </div>
-  );
-}
-
+  </div>
+);
 export default App;
