67d112588d4aec4c589e8363b4fdf31b1d0137eb
KC <krkeco@gmail.com>
2020-12-15 17:45:18 -0800

    button

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,9 +4,9 @@ const Card = ({small, name, href, x, y, onClick}) => {
     //href={href}
   return (
     <div onClick={onClick} style={{paddingTop:y, paddingLeft:x}}>
-        <a  className={`${small ? 'card small' : 'card'}` }>
+        <button  className={`${small ? 'card small' : 'card'}` }>
             <div>{name}</div>
-        </a>
+        </button>
     </div>
   );
 }
